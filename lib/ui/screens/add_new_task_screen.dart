aef30aba205ffeaa3c045f3771dcab33ebd08e60
rafiafsan <rafiafs45@gmail.com>
2025-04-13 02:57:18 +0600

    done

--- lib/ui/screens/add_new_task_screen.dart
+++ lib/ui/screens/add_new_task_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_fully_functional/data/service/network_client.dart';
+import 'package:task_manager_fully_functional/data/utils/urls.dart';
+import 'package:task_manager_fully_functional/ui/widgets/centered_circular_progress_ndicator.dart';
 import 'package:task_manager_fully_functional/ui/widgets/screen_background.dart';
+import 'package:task_manager_fully_functional/ui/widgets/snack_bar_message.dart';
 import 'package:task_manager_fully_functional/ui/widgets/tm_app_bar.dart';
 
 class AddNewTaskScreen extends StatefulWidget {
@@ -10,6 +14,12 @@ class AddNewTaskScreen extends StatefulWidget {
 }
 
 class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
+  final TextEditingController _titleTEController = TextEditingController();
+  final TextEditingController _descriptionTEController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _addNewTaskInProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,48 +28,67 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 32,
-                ),
-                Text(
-                  'Add New Task',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                TextFormField(
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(hintText: 'title'),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  maxLines: 6,
-                  decoration: InputDecoration(
-                    hintText: 'Description',
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 32,
                   ),
-                ),
-
-                const SizedBox(
-                  height: 24,
-                ),
-
-                ElevatedButton(
-                    onPressed: _onTapSubmitButton,
-                    child: Icon(
-                      Icons.arrow_circle_right_outlined,
-                      color: Colors.white,
-                      size: 30,
-                    )),
-
-              ],
+                  Text(
+                    'Add New Task',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    controller: _titleTEController,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(hintText: 'title'),
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your title';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _descriptionTEController,
+                    maxLines: 6,
+                    decoration: InputDecoration(
+                      hintText: 'Description',
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    ),
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your description';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Visibility(
+                    visible: _addNewTaskInProgress == false,
+                    replacement: CenteredCircularProgressNdicator(),
+                    child: ElevatedButton(
+                        onPressed: _onTapSubmitButton,
+                        child: Icon(
+                          Icons.arrow_circle_right_outlined,
+                          color: Colors.white,
+                          size: 30,
+                        )),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -67,7 +96,47 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
     );
   }
 
-  void _onTapSubmitButton(){
+  void _onTapSubmitButton() {
+    if (_formKey.currentState!.validate()) {
+      addNewTask();
+    }
+  }
 
+  Future<void> addNewTask() async {
+    _addNewTaskInProgress = true;
+    setState(() {});
+
+    Map<String, dynamic> requestBody = {
+      "title": _titleTEController.text.trim(),
+      "description": _descriptionTEController.text.trim(),
+      "status": "New"
+    };
+
+    final NetworkResponse response = await NetworkClient.postRequest(
+      url: Urls.createTaskUrl,
+      body: requestBody,
+    );
+
+    _addNewTaskInProgress = false;
+    setState(() {});
+
+    if (response.isSuccess) {
+      _clearTextFields();
+      showSnackBar(context, 'New Task Added!');
+    } else {
+      showSnackBar(context, response.errorMessage);
+    }
+  }
+
+  void _clearTextFields() {
+    _titleTEController.clear();
+    _descriptionTEController.clear();
+  }
+
+  @override
+  void dispose() {
+    _titleTEController.clear();
+    _descriptionTEController.clear();
+    super.dispose();
   }
 }
